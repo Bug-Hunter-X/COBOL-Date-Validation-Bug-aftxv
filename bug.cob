d169788e5b4266d8ca627eb5914eb7f3bb995252
BugHunter-X <askzenai@gmail.com>
2025-01-22 11:56:05 +0100

    Add bug.cob file

---
+++ bug.cob
@@ -0,0 +1,18 @@
+01  WS-AREA.          
+05  WS-DATE      PIC 9(8). 
+05  WS-TIME      PIC 9(6). 
+
+PROCEDURE DIVISION. 
+
+    DISPLAY "Enter date (YYYYMMDD):" 
+    ACCEPT WS-DATE 
+
+    DISPLAY "Enter time (HHMMSS):" 
+    ACCEPT WS-TIME 
+
+    IF WS-DATE > 20231231 THEN 
+        DISPLAY "Invalid date!" 
+    ELSE 
+        DISPLAY "Valid date and time." 
+    END-IF. 
+    STOP RUN.
